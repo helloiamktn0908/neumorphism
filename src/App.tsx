1f1ce3bc93b840cba9ba69ee01a201daf1e637b5
kotono.ukai <helloiamktn@gmail.com>
2021-10-24 10:12:01 +0900

    neumorphism化

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,48 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useState } from "react";
 import "./App.css";
 
+type ClickState = {
+  [key: number]: { isClicked: boolean };
+};
+
+const clickStates: ClickState = {
+  0: { isClicked: false },
+  1: { isClicked: false },
+  2: { isClicked: false },
+  3: { isClicked: false },
+  4: { isClicked: false },
+  5: { isClicked: false },
+  6: { isClicked: false },
+  7: { isClicked: false },
+};
+
 function App() {
+  const [isClicked, setIsClicked] = useState(clickStates);
+
+  const click = (e: any) => {
+    const target: number = e.currentTarget.dataset["index"];
+    const newIsClicked = {
+      ...isClicked,
+      [target]: { isClicked: !isClicked[target].isClicked },
+    };
+    setIsClicked(newIsClicked);
+  };
+
   return (
     <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React**
-        </a>
-      </header>
+      <div className='body'>
+        <div className='buttons'>
+          {Object.values(isClicked).map((clickState, i) => (
+            <div className='buttonWrapper'>
+              <button
+                className={clickState.isClicked ? "fuga" : "hoge"}
+                data-index={i}
+                onClick={click}
+              ></button>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
